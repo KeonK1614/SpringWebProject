f3f0494a8c0e997ac4ae14601254f4c4da506900
nayenoraeng <>
2024-08-14 15:21:45 +0900

    프로젝트명 변경 commit

---
+++ SpringBootProject_01/src/main/java/com/project/springboot/auth/AccountDetails.java
@@ -0,0 +1,64 @@
+package com.project.springboot.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.project.springboot.jdbc.UserDTO;
+
+public class AccountDetails implements UserDetails {
+
+	private static final long serialVersionUID = 1L; 
+	private UserDTO user;
+	
+	public AccountDetails(UserDTO user) {
+		this.user=user;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> collect = new ArrayList<>();
+		collect.add(new GrantedAuthority() {
+			@Override
+			public String getAuthority() {
+				return user.getIsAdmin();
+			}
+		});
+		return collect;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getId();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getPass();
+	}
+	
+	@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+	//계정 안 잠김?
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+	//계정 Credential 안 만료됨?
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+	
+    //비활성화 되지 않음? (오랫동안 사용하지 않은 경우 등)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ SpringBootProject_01/src/main/java/com/project/springboot/auth/WebSecurityConfig.java
@@ -0,0 +1,91 @@
+package com.project.springboot.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Autowired
+	public AuthenticationFailureHandler authenticationFailureHandler;
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf((csrf) -> csrf.disable())
+			.cors((cors) -> cors.disable())
+	    	.authorizeHttpRequests(request -> request
+	    		.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+	    		.requestMatchers("/").permitAll()
+//	    		.requestMatchers("/**").permitAll()
+	    		.requestMatchers("/guest/joinform").permitAll()
+//	    		.requestMatchers("/error").permitAll()
+	    		.requestMatchers("/static/**").permitAll()
+	            .requestMatchers("/assets/**", "/carousel/**","/css/**", "/js/**", "/img/**").permitAll()
+	            .requestMatchers("/guest/**").permitAll()  // 모두에게 허용.
+	            .requestMatchers("/security/**").permitAll() 
+	            .requestMatchers("/member/**").hasAnyRole("N", "Y")
+	            .requestMatchers("/admin/**").hasAnyRole("Y")
+	            .anyRequest().authenticated() // 어떠한 요청이라도 인증 필요 없음 ( anonymous() )  //authenticated()
+	        );
+    
+		http.formLogin((formLogin) -> formLogin
+				.loginPage("/security/loginform")
+				.permitAll()
+				.loginProcessingUrl("/security/loginform")
+		        .defaultSuccessUrl("/guest/main")
+//		        .failureHandler(authenticationFailureHandler)
+		        .usernameParameter("id")
+		        .passwordParameter("pass")
+		        .permitAll()
+				);
+		
+		http.logout((logout) -> logout
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/guest/main")
+			.invalidateHttpSession(true) // 세션 무효화
+            .deleteCookies("JSESSIONID")
+			.permitAll());
+  
+    
+    return http.build(); 
+		
+	}
+	
+	
+	public void configure(WebSecurity web) throws Exception {
+	    web.httpFirewall(defaultHttpFirewall());
+//	    web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+	 
+	//매핑 중 슬래시가 두개 이상들어갔을 때 처리해주는 것.
+	@Bean
+	public HttpFirewall defaultHttpFirewall() {
+	    return new DefaultHttpFirewall();
+	}
+	
+//	@Override
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+
+}
