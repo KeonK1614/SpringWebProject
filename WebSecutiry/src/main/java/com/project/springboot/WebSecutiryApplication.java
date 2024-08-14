cb90f7d733975d33673bb06c370bbbc4d32ecb40
nayenoraeng <>
2024-08-14 14:32:36 +0900

    userservice commit

---
+++ WebSecutiry/src/main/java/com/project/springboot/WebSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecutiryApplication.class, args);
+	}
+
+}
